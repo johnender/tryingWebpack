7799fb9190e1307b77fc90b4eb5d171cc80068a0
John Hernández <johnenderhernandez@gmail.com>
2021-03-09 21:01:43 -0600

    Worse commit ever

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # js-portfolio
+
+Add webpack to the project
+
+npm i webpack webpack-cli -D
+
+Or
+
+yarn add webpack webpack-cli -D
+
+
+Adding babel
+
+npm install -D babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime
+
+Or
+
+yarn add -D babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime
+
+
+Adding html-webpack
+
+npm i html-webpack-plugin -D
+
+Or
+
+yarn add html-webpack-plugin -D
+
+
+Adding css-loader
+
+npm i mini-css-extract-plugin css-loader -D
+
+Or
+
+yarn add mini-css-extract-plugin css-loader -D
+
+
+Adding stylus
+
+npm i stylus-loader -D
+
+Or 
+
+yarn add stylus-loader -D
+
+
+Adding copy plugin
+
+npm i copy-webpack-plugin -D
+
+Or
+
+yarn add copy-webpack-plugin -D
+
+
+
+Adding “file-loader” and “url-loader”
+
+npm install url-loader file-loader -D
+
+Or
+
+yarn add url-loader file-loader -D
+
+
+Adding css-minimizer-webpack-plugin and terser-webpack-plugin
+
+npm i css-minimizer-webpack-plugin terser-webpack-plugin -D
+
+
+Or
+
+yarn add css-minimizer-webpack-plugin terser-webpack-plugin -D
+
+
+Adding dotenv-webpack
+
+npm install -D dotenv-webpack
+
+Or
+
+yarn add -D dotenv-webpack
+
+
+Adding clean-webpack-plugin
+
+npm install -D clean-webpack-plugin
+
+Or
+
+yarn add -D clean-webpack-plugin
---
+++ webpack.config.dev.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: "./src/index.js",
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+  output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+    path: path.resolve(__dirname, "dist"),
+    // filename le pone el nombre al archivo final
+    filename: "./[name].[contenthash].js",
+    //Esta instrucción hace que webpack le agregue un hash
+    assetModuleFilename: 'assets/images/[hash][ext]'
+  },
+  mode: 'development',
+  resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+    extensions: [".js"],
+    alias: {
+      '@utils': path.resolve(__dirname, './src/utils/'),
+      '@templates': path.resolve(__dirname, './src/templates/'),
+      '@styles': path.resolve(__dirname, './src/styles/'),
+      '@images': path.resolve(__dirname, './src/assets/images/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader
+        test: /\.m?js$/,
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.(css|styl)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader' //loader para el preprocesador stylus
+        ]
+      },
+      {//regla para optimizar las imagenes
+        test: /\.png/,
+        type: 'asset/resource' 
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000, // O LE PASAMOS UN BOOLEANOS TRUE O FALSE
+            // Habilita o deshabilita la transformación de archivos en base64.
+            mimetype: 'aplication/font-woff',
+            // Especifica el tipo MIME con el que se alineará el archivo. 
+            // Los MIME Types (Multipurpose Internet Mail Extensions)
+            // son la manera standard de mandar contenido a través de la red.
+            name: "./[name].[contenthash].[ext]",
+            // EL NOMBRE INICIAL DEL ARCHIVO + SU EXTENSIÓN
+            // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria 
+            // ubuntu-regularhola.woff
+            outputPath: '../assets/fonts/', 
+            // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
+            publicPath: '../assets/fonts/',
+            // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
+
+            //si se pone './assets/fonts/', no carga las fuentes
+
+            esModule: false 
+            // AVISAR EXPLICITAMENTE SI ES UN MODULO
+          }
+        }
+      }
+    ]
+  },
+  // SECCION DE PLUGINS
+  plugins: [
+    new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+        inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
+        template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+        filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
+    }),
+    new MiniCssExtractPlugin({
+      filename: './assets/[name].[contenthash].css'
+    }),//plugin que pasa el css
+    new CopyPlugin({//plugin que sirve para copiar archivos o carpetas
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    }),
+		new Dotenv(),
+  ],
+}
